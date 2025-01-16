05b558719ce84713fcb791c1b8a4569a334fff02
Ginevra Bruschi <s11082774@liceodavincijesi.edu.it>
2025-01-16 09:58:21 +0100

    classe auto

--- auto.py
+++ auto.py
@@ -5,7 +5,46 @@
 #importare il file veicolo
 from veicolo import*
 
-#definire la classe auto numero massimo di passeggeri e numero di passeggeri trasportati,
+#definire la classe auto numero massimo di passeggeri e numero di passeggeri trasportati
 class Auto(Veicolo):
-    def __init__(self,numeroMassimoPasseggeri:int, passeggeriTrasportati:int):
+    def __init__(self, targa:str, numeroMassimoPasseggeri:int, passeggeriTrasportati:int):
+        super().__init__(targa)
         
+        #controllare il numero masimo di passeggeri
+        if numeroMassimoPasseggeri <= 0 or numeroMassimoPasseggeri >5:
+            raise ValueError ("il numero non è valido")
+        self.__numeroMassimoPasseggeri=numeroMassimoPasseggeri
+        
+        #controllare il numero di passeggeri trasportati
+        if passeggeriTrasportati < 0 or passeggeriTrasportati > (numeroMassimoPasseggeri-1):
+            raise ValueError ("il numero non è valido")
+        self.__passeggeriTrasportati = passeggeriTrasportati
+    
+    #-----------------------------------------------------------
+    #difinire le proprietà
+    @property
+    def numeroMassimoPasseggeri (self):
+        return self.__numeroMassimoPasseggeri
+    
+    @property
+    def passeggeriTrasportati (self):
+        return self.__passeggeriTrasportati
+    
+    #----------------------------------------------------------
+    #definire le setter
+    @numeroMassimoPasseggeri.setter
+    def numeroMassimoPasseggeri (self, value):
+        if value <= 0 or value > 5:
+            raise ValueError ("non valido")
+        self.__numeroMassimoPasseggeri=value
+        return
+    
+    @passeggeriTrasportati.setter
+    def passeggeriTrasportati (self, value):
+        if value < 0 or value > (numeroMassimoPasseggeri-1):
+            raise ValueError ("non valido")
+        self.__passeggeriTrasportati=value
+        return
+    #-----------------------------------------------------
+        
+    
